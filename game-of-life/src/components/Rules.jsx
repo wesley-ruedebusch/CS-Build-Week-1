9d28d124903afeb375a801c5de030c27ae0a488d
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-06-26 15:05:31 -0400

    finished up

---
+++ game-of-life/src/components/Rules.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Rules = () => {
+  return (
+    <div>
+      <h2>Rules:</h2>
+      <ol>
+        <li>
+          Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
+        </li>
+        <li>
+          Any live cell with two or three live neighbours lives on to the next
+          generation.
+        </li>
+        <li>
+          Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
+        </li>
+        <li>
+          Any dead cell with exactly three live neighbours becomes a live cell,
+          as if by reproduction.
+        </li>
+      </ol>{" "}
+    </div>
+  );
+};
+
+export default Rules;
